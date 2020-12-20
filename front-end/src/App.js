fb40ddbd2a2204c39e7bb4dc0d8fc444a4a64148
usmanGhani16 <afro123737@gmail.com>
2020-12-20 14:37:32 +0500

    finihshed setting up back end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      REACT AP P ?dadas???
-      <div>REACT AP P ?dadas???</div>
-    </div>
-  );
+  return <div className="App">REACT APp</div>;
 }
 
 export default App;
