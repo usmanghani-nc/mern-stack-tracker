f06b545798b7c403db61ef495121869dc8c7960e
usmanGhani16 <afro123737@gmail.com>
2020-12-20 14:25:30 +0500

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      REACT AP P ?dadas???
+      <div>REACT AP P ?dadas???</div>
+    </div>
+  );
+}
+
+export default App;
